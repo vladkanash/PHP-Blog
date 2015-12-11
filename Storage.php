973cae4748ad7a437e9bbd05c23eb5c53d6e0ac7
Vlad Kanash <vlad.kanash@gmail.com>
2015-12-11 21:51:19 +0300

    Initial commit.

---
+++ Storage.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: vladkanash
+ * Date: 12/11/15
+ * Time: 5:00 PM
+ */
+
+class Storage
+{
+
+
+}
---
+++ web/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vladkanash
+ * Date: 12/11/15
+ * Time: 9:48 PM
+ */
